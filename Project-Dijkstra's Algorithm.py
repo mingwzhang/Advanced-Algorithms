bf3d5333fc02446f6d94c987f58a258de90d60e2
Mingwei Zhang <81440878+mingwzhang@users.noreply.github.com>
2024-11-23 00:56:00 -0500

    Update Project-Dijkstra's Algorithm.py

--- Project-Dijkstra's Algorithm.py
+++ Project-Dijkstra's Algorithm.py
@@ -1,20 +1,34 @@
-import heapq  # Import the heap queue module to use a min-heap (priority queue)
+# Import the heap queue module to use a min-heap
+import heapq  
 
 def Dijkstra_Algorithm(graph, source):
-    distance = {node: float('inf') for node in graph}  # Create a new dictionay were all node distances are set to infinity
-    distance[source] = 0 # Set source or starting distance to 0
-    
+    # Create a new dictionay were all node distances are set to infinity
+    distance = {node: float('inf') for node in graph}
+    # Set source or starting distance to 0
+    distance[source] = 0 
+    # Priority queue will store tuples of (distance, node) to help select the closest node
     priority_queue = [(0, source)] 
-    
-    while priority_queue:  #The while loop will iterate until priority_queue is empty / every node in the heap are poped
-        current_distance, current_node = heapq.heappop(priority_queue) # 
+
+    # The while loop runs until all reachable nodes are processed (priority_queue is empty)
+    while priority_queue:
+        # Extract the node with the shortest known distance
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        # Iterate over all neighbors of the current node
         for neighbor, weight in graph[current_node]:
-            update_distance = current_distance + weight  #Setting update_distance based on current distance to neighbor distance
-            if update_distance < distance[neighbor]:  #Condition to update the distance if the new distance is shorter
+            # Calculate the new tentative distance to the neighbor traveling the current node
+            update_distance = current_distance + weight  
+
+            # Condition to update the distance if the new distance is shorter
+            if update_distance < distance[neighbor]:
+                # Update the shortest distance for the neighbor
                 distance[neighbor] = update_distance
+
+                # Add the neighbor to the priority queue with the updated distance
                 heapq.heappush(priority_queue, (update_distance, neighbor))
     print(distance)
-    
+
+# Graph Representation Input:
 graph = {
     'A': [('B', 3), ('C', 2), ('D', 8)],
     'B': [('C', 1), ('E', 3)],
@@ -23,11 +37,13 @@ graph = {
     'E': []
 }
 
+# Print the graph representation to provide context before running the algorithm
 print()
 print("Graph")
 for node in graph:
     print(f'{node}: {graph[node]}')
 
+# Find and print the shortest path from the source node 'A' to all other nodes
 print()
 print("Shortest Path For Each Node:")
 Dijkstra_Algorithm(graph, 'A')
